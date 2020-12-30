5e8fad0523d57df078fc7140ac6b4d2009cc56b4
Jerome Caruso <jerome0caruso@gmail.com>
2020-12-30 17:30:47 -0600

    Updates

---
+++ precache-manifest.dc2242d7847bf359432ada9e2fa7c18d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7143688d7e105217d141ba31f6b431",
+    "url": "/jerome0caruso/resturantReachSite/index.html"
+  },
+  {
+    "revision": "c0e0d5c727469f0bae9e",
+    "url": "/jerome0caruso/resturantReachSite/static/css/2.be08b330.chunk.css"
+  },
+  {
+    "revision": "8a973b51f5ced54dcb23",
+    "url": "/jerome0caruso/resturantReachSite/static/css/main.be0b3f75.chunk.css"
+  },
+  {
+    "revision": "c0e0d5c727469f0bae9e",
+    "url": "/jerome0caruso/resturantReachSite/static/js/2.016ad94d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jerome0caruso/resturantReachSite/static/js/2.016ad94d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a973b51f5ced54dcb23",
+    "url": "/jerome0caruso/resturantReachSite/static/js/main.a93d82f6.chunk.js"
+  },
+  {
+    "revision": "0f63f953d6428aa86b39",
+    "url": "/jerome0caruso/resturantReachSite/static/js/runtime-main.d16a719f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/jerome0caruso/resturantReachSite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/jerome0caruso/resturantReachSite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/jerome0caruso/resturantReachSite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/jerome0caruso/resturantReachSite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/jerome0caruso/resturantReachSite/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
